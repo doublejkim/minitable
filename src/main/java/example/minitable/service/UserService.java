8afb0e9f8dadb013732cce1feb18dcdf503701ad
doublejkim <jaejin.km@gmail.com>
2022-03-22 00:23:20 +0900

    feat : UserService 에서 회원가입에 관련된 메소드 삭제 (SignUpSevice 로 이동)

--- src/main/java/example/minitable/service/UserService.java
+++ src/main/java/example/minitable/service/UserService.java
@@ -1,11 +1,8 @@
 package example.minitable.service;
 
 import example.minitable.domain.User;
-import example.minitable.dto.UserDto;
-import example.minitable.exception.AlreadyRegisteredUserException;
 import example.minitable.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,28 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public User signupCustomer(UserDto userDto) {
-
-        if(userRepository.findByEmail(userDto.getEmail()) != null) {
-            throw new AlreadyRegisteredUserException();
-        }
-
-        // authority 설정
-        userDto.setAuthority("ROLE_CUSTOMER");
-
-        // 패스워드 암호화
-        String encryptPwd = passwordEncoder.encode(userDto.getPassword());
-        userDto.setPassword(encryptPwd);
-
-
-        return userRepository.save(User.from(userDto));
-
-    }
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
-
 }
