4a4cf846ad2e3346c37985fafff3457b9aad401f
doublejkim <jaejin.km@gmail.com>
2022-03-22 00:25:24 +0900

    feat : RestaurantRepository 개발

---
+++ src/main/java/example/minitable/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package example.minitable.repository;
+
+import example.minitable.domain.store.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+
+}
