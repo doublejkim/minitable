19a93ba79d970df958d315c3e8b41a1955830019
doublejkim <jaejin.km@gmail.com>
2022-03-25 15:50:42 +0900

    feat : api 호출용 예약컨트롤러 개발

---
+++ src/main/java/example/minitable/controller/api/ApiBookingController.java
@@ -0,0 +1,54 @@
+package example.minitable.controller.api;
+
+import example.minitable.dto.ApiDataResponse;
+import example.minitable.dto.BookingDto;
+import example.minitable.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+public class ApiBookingController {
+
+    private final BookingService bookingService;
+
+    @GetMapping("/api/bookinglist")
+    public ApiDataResponse<Page<BookingDto>> getBookingList(Pageable pageable) {
+
+        int page = (pageable.getPageNumber() ==0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10); // @@@
+
+        Page<BookingDto> resultPage = bookingService.getBookingList(pageable);
+
+        return ApiDataResponse.of(resultPage);
+
+    }
+
+    @GetMapping("/api/bookinglist-with-email")
+    public ApiDataResponse<Page<BookingDto>> getBookingListWithStoreOwnerEmail(
+            @NotEmpty String email,
+            Pageable pageable)
+    {
+
+        int page = (pageable.getPageNumber() ==0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10); // @@@
+
+        Page<BookingDto> resultPage = bookingService.getBookingListWithStoreOwnerEmail(email, pageable);
+
+        return ApiDataResponse.of(resultPage);
+
+    }
+}
